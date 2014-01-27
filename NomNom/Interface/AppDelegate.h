f7eb9e1f51b67a5f0652c51670e46fc8e8f17009
Jake Gregg <greggjs@miamioh.edu>
2014-01-27 14:53:19 -0500

    Added Core data stuff

--- NomNom/Interface/AppDelegate.h
+++ NomNom/Interface/AppDelegate.h
@@ -7,10 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ViewController.h"
+#import "User.h"
+#import "FriendsViewController.h"
+#import "GameViewController.h"
+#import "HomeViewController.h"
+#include <unistd.h>
+#include <netdb.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic) UITabBarController *tabBarController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext* managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel* managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator* persistentStoreCoordinator;
+
+- (void) saveContext;
+- (NSURL *) applicationDocumentsDirectory;
+
+-(BOOL) isNetworkAvailable;
 
 @end
